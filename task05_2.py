34b74eda8c601e414baaf940a7c5e2d9b82bacf4
Oleksandr Golubchyk <81289118+GOS81@users.noreply.github.com>
2025-06-25 23:41:11 +0300

    added tabs

--- task05_2.py
+++ task05_2.py
@@ -10,12 +10,8 @@ def generator_numbers(text_str):
     for match in re.findall(pattern, text_str):
         yield float(match)
 
-
-
-
 def sum_profit(text: str, func: Callable[[str], Generator[float]]):
     return sum(func(text))
-
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
